7ab4a9a93ff710b8d5df9e183719168b76b52bbd
nkjm <nkjm.kzk@gmail.com>
2017-02-02 20:58:03 +0900

    changed sessionId to userId

--- action/show-menu.js
+++ action/show-menu.js
@@ -95,7 +95,7 @@ module.exports = class ActionShowMenu {
 
                     messages = [{
                         type: "text",
-                        text: this._conversation.confirmed.date + "のPLATE Aは" + food_list.a.menu + "です。"
+                        text: that._conversation.confirmed.date + "のPLATE Aは" + food_list.a.menu + "です。"
                     }];
                 }
 
