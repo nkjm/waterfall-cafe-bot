d65bf9cd94fd2ae59b134642428f200f2e6b769f
nkjm <nkjm.kzk@gmail.com>
2017-02-13 20:25:24 +0900

    separate flow to /flow

--- flow/start_conversation.js
+++ flow/start_conversation.js
@@ -21,54 +21,55 @@ module.exports = class StartConversationFlow {
     run(){
         console.log("\n### This is Start Conversation Flow. ###\n");
         let that = this;
-        return new Promise(function(resolve, reject){
-            // "message" is the only supported event on starting conversation.
-            if (that.line_event.type != "message"){
-                console.log("Not supported event type in this flow.");
-                return resolve();
-            }
 
-            // Indentify Intent.
-            flow_tool.identify_intent(that.line_event.source.userId, that.line_event.message.text).then(
-                function(response){
-                    console.log("Intent is " + response.result.action);
+        // "message" is the only supported event on starting conversation.
+        if (that.line_event.type != "message"){
+            console.log("Not supported event type in this flow.");
+            return new Promise(function(resolve, reject){
+                resolve();
+            });
+        }
 
-                    // Instantiate the conversation object. This will be saved as Bot Memory.
-                    that.conversation.intent = response.result;
+        // Indentify Intent.
+        return flow_tool.identify_intent(that.line_event.source.userId, that.line_event.message.text).then(
+            function(response){
+                console.log("Intent is " + response.result.action);
 
-                    /*
-                    ** Instantiate action depending on the intent.
-                    ** The implementations of each action are located under /action directory.
-                    */
-                    that.action = flow_tool.instantiate_action(that.conversation.intent.action);
-                    that.conversation.to_confirm = flow_tool.identify_to_confirm_parameter(that.action.required_parameter, that.conversation.confirmed);
+                // Instantiate the conversation object. This will be saved as Bot Memory.
+                that.conversation.intent = response.result;
 
-                    /*
-                    ** If api.ai return some parameters. we save them in conversation object so that Bot can remember.
-                    */
-                    if (that.conversation.intent.parameters && Object.keys(that.conversation.intent.parameters).length > 0){
-                        for (let param_key of Object.keys(that.conversation.intent.parameters)){
-                            let parameter = {};
-                            parameter[param_key] = that.conversation.intent.parameters[param_key];
-                            parameter = that.action.parse_parameter(parameter);
+                /*
+                ** Instantiate action depending on the intent.
+                ** The implementations of each action are located under /action directory.
+                */
+                that.action = flow_tool.instantiate_action(that.conversation.intent.action);
+                that.conversation.to_confirm = flow_tool.identify_to_confirm_parameter(that.action.required_parameter, that.conversation.confirmed);
 
-                            if (parameter){
-                                flow_tool.add_parameter(that.conversation, parameter);
-                            }
+                /*
+                ** If api.ai return some parameters. we save them in conversation object so that Bot can remember.
+                */
+                if (that.conversation.intent.parameters && Object.keys(that.conversation.intent.parameters).length > 0){
+                    for (let param_key of Object.keys(that.conversation.intent.parameters)){
+                        let parameter = {};
+                        parameter[param_key] = that.conversation.intent.parameters[param_key];
+                        parameter = that.action.parse_parameter(parameter);
+
+                        if (parameter){
+                            flow_tool.add_parameter(that.conversation, parameter);
                         }
                     }
-
-                    /*
-                    ** Run the intent oriented action.
-                    ** This may lead collection of another parameter or final action for this intent.
-                    */
-                    return flow_tool.run(that.action, that.line_event, that.conversation);
-                },
-                function(response){
-                    console.log("Failed to indentify intent.");
-                    return reject(response);
                 }
-            );
-        }); // End of Return new Promise()
+
+                /*
+                ** Run the intent oriented action.
+                ** This may lead collection of another parameter or final action for this intent.
+                */
+                return flow_tool.run(that.action, that.line_event, that.conversation);
+            },
+            function(response){
+                console.log("Failed to indentify intent.");
+                return Promise.reject(response);
+            }
+        );
     } // End of run()
 };
